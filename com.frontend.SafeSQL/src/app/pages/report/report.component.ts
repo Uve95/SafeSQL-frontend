5beddb4dfa7bba5fd2753742eafcecfc9150e164
Uve95 <91894766+Uve95@users.noreply.github.com>
2023-10-30 15:01:12 +0100

    +detalles vulnerabilidades del reporte

--- com.frontend.SafeSQL/src/app/pages/report/report.component.ts
+++ com.frontend.SafeSQL/src/app/pages/report/report.component.ts
@@ -544,7 +544,8 @@ export class ReportComponent implements OnInit {
       })
       .replace(/<button.*?<\/button>/g, '') // Eliminar las etiquetas <button>
       .replace("undefined", '') // Eliminar las etiquetas <a>
-      .replace("SafeSQL", '');// Eliminar las etiquetas <a>
+      .replace("SafeSQL", '')// Eliminar las etiquetas <a>
+      .replace(/<p[^>]*>.*?<\/p>/g, '') // Eliminar las etiquetas <p> y su contenido
 
 
 
@@ -579,7 +580,9 @@ export class ReportComponent implements OnInit {
       })
       .replace(/<button.*?<\/button>/g, '') // Eliminar las etiquetas <button>
       .replace("undefined", '') // Eliminar las etiquetas <a>
-      .replace("SafeSQL", '');// Eliminar las etiquetas <a>
+      .replace("SafeSQL", '')// Eliminar las etiquetas <a>
+      .replace(/<p[^>]*>.*?<\/p>/g, '') // Eliminar las etiquetas <p> y su contenido
+
 
 
     // Crear un enlace para descargar el HTML modificado
@@ -613,89 +616,110 @@ export class ReportComponent implements OnInit {
   }
 
   details(vulnerability:string) {
-    const autentication = "La autenticación de Windows es más segura que la autenticación de SQL Server, ya que aprovecha la seguridad integrada en el sistema operativo. Utiliza la autenticación de Windows para las conexiones a SQL Server siempre que sea posible.";
-    
-    
+    const c1 = "La autenticación de Windows es más segura que la autenticación de SQL Server, ya que aprovecha la seguridad integrada en el sistema operativo. Utiliza la autenticación de Windows para las conexiones a SQL Server siempre que sea posible.";
+    const c2 = "Comprobar que nadie haya dejado una puerta trasera. Son procedimientos que se arrancan automáticamente con el sistema."
+    const c3 = "Los procedimientos almacenados peligrosos en SQL Server suelen ser aquellos que, si se ejecutan de manera inapropiada o sin la debida autorización, pueden tener un impacto negativo en la seguridad, la integridad de los datos o el rendimiento del sistema. Ejemplos(sp_configure, sp_addloggin,sp_droplogin..)"
+    const c4 ="La falta de auditorías de acceso en SQL Server puede resultar en una pérdida de visibilidad sobre las actividades en la base de datos, dificultando la detección de intrusiones y la investigación de incidentes. Además, puede llevar a incumplimientos normativos y riesgos de seguridad no detectados, así como dificultar la gestión de cambios y la trazabilidad de las acciones en la base de datos."
+    const c5 = "La actualización de SQL Server es crucial para mitigar riesgos de seguridad, garantizar el cumplimiento normativo y optimizar el rendimiento de la base de datos."
+    const c6 ="El cifrado de conexiones entrantes es esencial para proteger la seguridad de los datos y garantizar la confidencialidad durante la transferencia. La falta de cifrado puede exponer la información a riesgos de interceptación y acceso no autorizado."
+    const c7 = "La autenticación anónima permite el acceso sin autenticación. Deshabilitarla en SQL Server es crucial, ya que puede exponer datos sensibles a accesos no autorizados. Utiliza autenticación sólida, como la autenticación de Windows o la autenticación de SQL Server, para garantizar que los usuarios estén debidamente autenticados antes de acceder a la base de datos."
+    const c8 = "El uso de puertos no estándar puede dificultar la administración y la seguridad. Se recomienda utilizar los puertos estándar (por ejemplo, 1433 para SQL Server) para garantizar la interoperabilidad y reducir la exposición a vulnerabilidades. Evitar puertos no estándar simplifica la administración y mantiene la seguridad."
+    const c9 ="La implementación de conexiones cifradas con SSL/TLS es fundamental para proteger la confidencialidad de los datos durante la transferencia. SSL/TLS encripta los datos, lo que impide que terceros intercepten la información en tránsito. Asegúrate de configurar y habilitar SSL/TLS en SQL Server para proteger las conexiones remotas."
+    const c10 ="Las conexiones a SQL Server deben utilizar métodos de autenticación seguros. Evita el acceso anónimo y utiliza autenticación sólida, como la autenticación de Windows o la autenticación de SQL Server, para garantizar que los usuarios sean quienes dicen ser y tengan permiso para acceder a la base de datos."
+    const c11="El usuario guest es un usuario predeterminado que a menudo tiene permisos limitados en las bases de datos. Es importante revisar y restringir los permisos de este usuario, ya que podría ser un punto de acceso no deseado a la base de datos. Limita o elimina sus permisos según sea necesario."
+    const c12=" La función de base de datos public contiene a todos los usuarios con acceso a la base de datos. Evita otorgar permisos directamente a esta función, ya que esto podría dar acceso a usuarios no deseados. En su lugar, asigna permisos a roles específicos y usuarios individuales para un control más preciso."
+    const c13="Establece políticas de contraseñas sólidas que requieran contraseñas complejas con combinaciones de letras, números y caracteres especiales. Además, implementa un período de caducidad y exige cambios de contraseñas periódicos para evitar vulnerabilidades debidas a contraseñas débiles."
+    const c14="Realiza auditorías regulares de contraseñas para identificar contraseñas débiles o comprometidas. Cambia y refuerza las contraseñas no seguras para mantener la seguridad."
+    const c15="Implementa políticas de inicio de sesión seguras que incluyan bloqueo de cuentas después de un número específico de intentos fallidos. Esto protege contra ataques de fuerza bruta y aumenta la seguridad del inicio de sesión."
+    const c16="Elimina o deshabilita los inicios de sesión que ya no son necesarios. Los inicios de sesión no utilizados pueden representar un riesgo potencial y aumentar la superficie de ataque."
+    const c17="Monitoriza y registra los inicios de sesión fallidos para detectar posibles intentos de acceso no autorizado. Establece alertas para notificar a los administradores sobre actividades sospechosas."
+    const c18="Crea y sigue un plan de mantenimiento regular que incluya copias de seguridad, actualizaciones de índices y estadísticas, y otras tareas de mantenimiento para mantener el rendimiento y la integridad de la base de datos."
+    const c19="Mantén copias de seguridad actualizadas y verifica la integridad de los archivos de respaldo. Las copias de seguridad son esenciales para la recuperación ante desastres y la protección de los datos."
+    const c20="Gestionar la fragmentación de índices es crucial para mantener un rendimiento óptimo. Realiza mantenimiento de índices para reducir la fragmentación y mejorar la eficiencia de las consultas."
+    const c21="Supervisa y ajusta consultas o procesos que consumen excesiva CPU para mejorar la eficiencia del sistema y garantizar un uso equitativo de recursos."
+    const c22="Protege datos sensibles mediante cifrado y restricciones de acceso basadas en roles. Implementa políticas de seguridad de datos para proteger la confidencialidad y la integridad de la información."
+    const c23="Utiliza cifrado para proteger datos confidenciales durante su almacenamiento y transmisión. SQL Server ofrece opciones de cifrado, como Transparent Data Encryption (TDE), para garantizar la seguridad de los datos."
+    const c24="Limita el número de miembros con el rol sysadmin, que tiene privilegios amplios en SQL Server. Restringe el acceso a este rol para reducir los riesgos de seguridad."
+    const c25 =" Asigna roles fijos db_datareader y db_datawriter con precaución, ya que otorgan permisos de lectura y escritura en todas las tablas de la base de datos. Limita el uso de estos roles para evitar accesos innecesarios a los datos."
+
     const popup = window.open('', 'Detalles',  'width=auto,height=auto,resizable=no,scrollbars=no');
       
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c1"){
+        popup.document.body.innerHTML = `<p>${c1}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c2"){
+        popup.document.body.innerHTML = `<p>${c2}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c3"){
+        popup.document.body.innerHTML = `<p>${c3}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c4"){
+        popup.document.body.innerHTML = `<p>${c4}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c5"){
+        popup.document.body.innerHTML = `<p>${c5}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c6"){
+        popup.document.body.innerHTML = `<p>${c6}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c7"){
+        popup.document.body.innerHTML = `<p>${c7}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c8"){
+        popup.document.body.innerHTML = `<p>${c8}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c9"){
+        popup.document.body.innerHTML = `<p>${c9}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c10"){
+        popup.document.body.innerHTML = `<p>${c10}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c11"){
+        popup.document.body.innerHTML = `<p>${c11}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c12"){
+        popup.document.body.innerHTML = `<p>${c12}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c13"){
+        popup.document.body.innerHTML = `<p>${c13}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c14"){
+        popup.document.body.innerHTML = `<p>${c14}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c15"){
+        popup.document.body.innerHTML = `<p>${c15}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c16"){
+        popup.document.body.innerHTML = `<p>${c16}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c17"){
+        popup.document.body.innerHTML = `<p>${c17}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c18"){
+        popup.document.body.innerHTML = `<p>${c18}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c19"){
+        popup.document.body.innerHTML = `<p>${c19}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c20"){
+        popup.document.body.innerHTML = `<p>${c20}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c21"){
+        popup.document.body.innerHTML = `<p>${c21}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c22"){
+        popup.document.body.innerHTML = `<p>${c22}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c23"){
+        popup.document.body.innerHTML = `<p>${c23}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c24"){
+        popup.document.body.innerHTML = `<p>${c24}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
+      if(vulnerability=="c25"){
+        popup.document.body.innerHTML = `<p>${c25}</p>`;
       }
-      if(vulnerability=="autentication"){
-        popup.document.body.innerHTML = `<p>${autentication}</p>`;
-      }
+
 
     }
   }
